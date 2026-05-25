a4c92ad9b3ee0b4432c674197b703b4a6c620273
alexchebur <93209745+alexchebur@users.noreply.github.com>
2026-05-25 20:11:13 +0500

    Create entity.js

---
+++ entity.js
@@ -0,0 +1,100 @@
+// =========================== Модуль сущностей (игрок, враги, предметы) ===========================
+const EntityModule = (function() {
+    function createPlayer(x, y) {
+        return {
+            x: x, y: y,
+            char: "@", color: "#FFF",
+            hp: 5000, maxHp: 5000,
+            atk: 5, def: 2,
+            level: 1, xp: 0,
+            inventory: [],
+            equipment: { weapon: null, armor: null }
+        };
+    }
+
+    function createEnemy(template, x, y, difficultyMult) {
+        const hp = Math.floor(template.hp[0] * difficultyMult);
+        const atk = Math.floor(template.atk[0] * difficultyMult);
+        const def = Math.floor(template.def[0] * difficultyMult);
+
+        return {
+            x: x, y: y, name: template.name,
+            char: template.char, color: template.color,
+            hp: hp, maxHp: hp,
+            atk: atk, def: def,
+            isEnemy: true
+        };
+    }
+
+    function createItem(template, x, y, itemPowerMult) {
+        const adj = DataModule.ITEM_ADJECTIVES[Math.floor(Math.random() * DataModule.ITEM_ADJECTIVES.length)];
+        const name = `${adj} ${template.baseName}`;
+
+        const baseVal = Math.floor(template.val[0] + Math.random() * (template.val[1] - template.val[0]));
+        const finalVal = Math.max(1, Math.floor(baseVal * itemPowerMult));
+
+        return {
+            x: x, y: y, name: name,
+            char: template.char, color: template.color,
+            type: template.type,
+            stat: template.stat,
+            effect: template.effect,
+            val: finalVal,
+            isItem: true
+        };
+    }
+
+    // Безопасное размещение врагов с проверкой дистанции
+    function spawnEnemies(mapGrid, startPos, enemyTemplates, count, difficultyMult, minDist = 3) {
+        const height = mapGrid.length;
+        const width = mapGrid[0].length;
+        const validTiles = [];
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                if (mapGrid[y][x] === 0) {
+                    const distToStart = Math.abs(x - startPos.x) + Math.abs(y - startPos.y);
+                    if (distToStart >= 4) {
+                        validTiles.push({ x, y });
+                    }
+                }
+            }
+        }
+
+        // Fisher-Yates shuffle
+        for (let i = validTiles.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [validTiles[i], validTiles[j]] = [validTiles[j], validTiles[i]];
+        }
+
+        const placedEnemies = [];
+        const occupiedCoords = [];
+
+        for (const tile of validTiles) {
+            if (placedEnemies.length >= count) break;
+
+            let tooClose = false;
+            for (const occ of occupiedCoords) {
+                if (Math.abs(tile.x - occ.x) + Math.abs(tile.y - occ.y) < minDist) {
+                    tooClose = true;
+                    break;
+                }
+            }
+
+            if (!tooClose) {
+                occupiedCoords.push({ x: tile.x, y: tile.y });
+                const template = enemyTemplates[Math.floor(Math.random() * enemyTemplates.length)];
+                placedEnemies.push(createEnemy(template, tile.x, tile.y, difficultyMult));
+            }
+        }
+
+        return placedEnemies;
+    }
+
+    return {
+        createPlayer,
+        createEnemy,
+        createItem,
+        spawnEnemies
+    };
+})();
