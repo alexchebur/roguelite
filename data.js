548cc66bd67b502b12df48b2ef261b7432a7565a
alexchebur <93209745+alexchebur@users.noreply.github.com>
2026-05-28 18:15:44 +0500

    Update data.js

--- data.js
+++ data.js
@@ -13,32 +13,28 @@ const DataModule = (function() {
     ];
 
     const ENEMY_TYPES = [
-        // === УРОВЕНЬ 1-3: Обучение и разминка ===
-        // Игрок (HP 100, Def 3) должен чувствовать себя уверенно.
-        { name: "Крыса", char: "r", color: "#795548", hp: [8, 12], atk: [1, 1], def: [0, 0] },      // Урон 0-1 (безопасно)
-        { name: "Гоблин", char: "g", color: "#4CAF50", hp: [12, 18], atk: [1, 2], def: [0, 1] },    // Урон 1-3 (легко)
-        { name: "Волк", char: "w", color: "#9E9E9E", hp: [15, 22], atk: [2, 3], def: [0, 1] },      // Урон 2-4 (средне)
+        // === УРОВЕНЬ 1-3: Животные (Еда) и Гоблины (Золото/Оружие) ===
+        { name: "Крыса", char: "r", color: "#795548", hp: [8, 12], atk: [1, 1], def: [0, 0], lootType: "food" },
+        { name: "Гоблин", char: "g", color: "#4CAF50", hp: [12, 18], atk: [1, 2], def: [0, 1], lootType: "gold" },
+        { name: "Волк", char: "w", color: "#9E9E9E", hp: [15, 22], atk: [2, 3], def: [0, 1], lootType: "food" },
         
-        // === УРОВЕНЬ 4-6: Первая опасность ===
-        // Появляются враги с защитой или высоким уроном.
-        { name: "Бандит", char: "b", color: "#FF9800", hp: [25, 35], atk: [3, 5], def: [1, 2] },    // Танкует немного
-        { name: "Скелет", char: "s", color: "#B0BEC5", hp: [20, 30], atk: [3, 6], def: [1, 2] },    // Бьет больнее
-        { name: "Слизень", char: "j", color: "#00BCD4", hp: [30, 45], atk: [2, 3], def: [3, 5] },   // Почти не получает урон без хорошего оружия
-        { name: "Орк-разведчик", char: "O", color: "#8BC34A", hp: [35, 50], atk: [4, 7], def: [2, 3] }, // Опасен в группе
+        // === УРОВЕНЬ 4-6: Человекоподобные (Оружие/Золото) и Монстры ===
+        { name: "Бандит", char: "b", color: "#FF9800", hp: [25, 35], atk: [3, 5], def: [1, 2], lootType: "weapon" },
+        { name: "Скелет", char: "s", color: "#B0BEC5", hp: [20, 30], atk: [3, 6], def: [1, 2], lootType: "gold" },
+        { name: "Слизень", char: "j", color: "#00BCD4", hp: [30, 45], atk: [2, 3], def: [3, 5], lootType: "food" }, // Слизь съедобна? Пусть будет еда :)
+        { name: "Орк-разведчик", char: "O", color: "#8BC34A", hp: [35, 50], atk: [4, 7], def: [2, 3], lootType: "weapon" },
 
         // === УРОВЕНЬ 7-9: Серьезные противники ===
-        // Требуют зелий и хорошей экипировки.
-        { name: "Зомби", char: "z", color: "#607D8B", hp: [50, 70], atk: [6, 9], def: [2, 4] },     // Медленный, но живой
-        { name: "Гарпия", char: "h", color: "#E91E63", hp: [40, 60], atk: [8, 12], def: [1, 2] },   // Высокий урон, мало HP
-        { name: "Призрак", char: "G", color: "#7C4DFF", hp: [30, 45], atk: [7, 10], def: [0, 1] },  // Бьет больно, но хрупкий
-        { name: "Вампир", char: "V", color: "#C62828", hp: [60, 85], atk: [9, 13], def: [3, 5] },   // Универсальный боец
+        { name: "Зомби", char: "z", color: "#607D8B", hp: [50, 70], atk: [6, 9], def: [2, 4], lootType: "gold" },
+        { name: "Гарпия", char: "h", color: "#E91E63", hp: [40, 60], atk: [8, 12], def: [1, 2], lootType: "weapon" },
+        { name: "Призрак", char: "G", color: "#7C4DFF", hp: [30, 45], atk: [7, 10], def: [0, 1], lootType: "gold" },
+        { name: "Вампир", char: "V", color: "#C62828", hp: [60, 85], atk: [9, 13], def: [3, 5], lootType: "weapon" },
 
         // === УРОВЕНЬ 10+: Боссы и элита ===
-        // Без полного сета брони и оружия лучше не лезть.
-        { name: "Тролль", char: "T", color: "#4CAF50", hp: [80, 120], atk: [10, 15], def: [2, 3] }, // Регенерация (в будущем), сейчас просто танк
-        { name: "Лич", char: "L", color: "#7B1FA2", hp: [70, 100], atk: [12, 18], def: [2, 4] },    // Магический урон
-        { name: "Голем", char: "M", color: "#90A4AE", hp: [120, 180], atk: [12, 16], def: [8, 12] }, // Стена. Нужен пробив брони.
-        { name: "Дракон", char: "D", color: "#FF5722", hp: [100, 150], atk: [15, 22], def: [5, 8] } // Финальный босс
+        { name: "Тролль", char: "T", color: "#4CAF50", hp: [80, 120], atk: [10, 15], def: [2, 3], lootType: "gold" },
+        { name: "Лич", char: "L", color: "#7B1FA2", hp: [70, 100], atk: [12, 18], def: [2, 4], lootType: "weapon" },
+        { name: "Голем", char: "M", color: "#90A4AE", hp: [120, 180], atk: [12, 16], def: [8, 12], lootType: "gold" },
+        { name: "Дракон", char: "D", color: "#FF5722", hp: [100, 150], atk: [15, 22], def: [5, 8], lootType: "weapon" }
     ];
     const ITEM_TYPES = [
         // === МЕЛЕЕ ОРУЖИЕ (meleeType: true) ===
@@ -61,6 +57,9 @@ const DataModule = (function() {
         { type: "armor", char: "{", color: "#8D6E63", baseName: "Плащ теней", stat: "def", val: [2, 3], gender: "he", plural: false },
         { type: "armor", char: "H", color: "#607D8B", baseName: "Шлем", stat: "def", val: [1, 2], gender: "he", plural: false },
         { type: "armor", char: "G", color: "#8D6E63", baseName: "Перчатки", stat: "def", val: [1, 2], gender: "she", plural: true },
+
+        // === ЗОЛОТО (Новый тип) ===
+        { type: "gold", char: "$", color: "#FFD700", baseName: "Монеты", val: [5, 15] },
         
         // === ЗЕЛЬЯ И ЕДА (effect: "heal" или "buff_atk") ===
         { type: "potion_hp", char: "!", color: "#f44336", baseName: "Зелье лечения", effect: "heal", val: [10, 20] },
