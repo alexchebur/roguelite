02e9e79e97f280d49f9ffde955d6c8a24dd9b52e
alexchebur <93209745+alexchebur@users.noreply.github.com>
2026-06-01 16:57:34 +0500

    Update tileset_renderer.js

--- tileset_renderer.js
+++ tileset_renderer.js
@@ -9,8 +9,8 @@ const TilesetRenderer = (function() {
     // Убедитесь, что координаты x,y соответствуют вашему PNG!
     const TILE_MAP = {
         //  Terrain (ПОПРОБУЙТЕ СДВИНУТЬ Y НА 1, ЕСЛИ ПЕРВЫЙ РЯД ПУСТОЙ)
-        '#': { file: 'terrain', x: 1, y: 3 }, 
-        '.': { file: 'terrain', x: 1, y: 1 },
+        '#': { file: 'terrain', x: 1, y: 2 }, 
+        '.': { file: 'terrain', x: 0, y: 1 },
         '>': { file: 'terrain', x: 3, y: 0 },
         '<': { file: 'terrain', x: 2, y: 0 },
         'T': { file: 'terrain', x: 8, y: 2 },
