f93603522726d16f3e7a5db01a7c5cf79cfe3118
alexchebur <93209745+alexchebur@users.noreply.github.com>
2026-06-05 16:04:33 +0500

    Update entity.js

--- entity.js
+++ entity.js
@@ -15,29 +15,29 @@ const EntityModule = (function() {
 
 // В файле entity.js, функция createEnemy
 
-    function createEnemy(template, x, y, difficultyMult) {
-        const hp = Math.floor(((template.hp[0] + template.hp[1]) / 2) * difficultyMult);
-        const atk = Math.floor(((template.atk[0] + template.atk[1]) / 2) * difficultyMult);
-        const def = Math.floor(((template.def[0] + template.def[1]) / 2) * difficultyMult);
+function createEnemy(template, x, y, difficultyMult) {
+    const hp = Math.floor(((template.hp[0] + template.hp[1]) / 2) * difficultyMult);
+    const atk = Math.floor(((template.atk[0] + template.atk[1]) / 2) * difficultyMult);
+    const def = Math.floor(((template.def[0] + template.def[1]) / 2) * difficultyMult);
     
-        // Добавляем скорость и энергию
-        const speed = template.speed || 10; 
-        // Начальная энергия случайна от 0 до speed, чтобы рассинхронизировать толпу
-        const startEnergy = Math.floor(Math.random() * speed); 
+    // Добавляем скорость и энергию
+    const speed = template.speed || 10; 
+    // Начальная энергия случайна от 0 до speed, чтобы рассинхронизировать толпу
+    const startEnergy = Math.floor(Math.random() * speed); 
 
-        return {
-            x: x, y: y, name: template.name,
-            char: template.char, color: template.color,
-            hp: hp, maxHp: hp,
-            atk: atk, def: def,
-            isEnemy: true,
-            lootType: template.lootType,
+    return {
+        x: x, y: y, name: template.name,
+        char: template.char, color: template.color,
+        hp: hp, maxHp: hp,
+        atk: atk, def: def,
+        isEnemy: true,
+        lootType: template.lootType,
         
-            // Новые поля:
-            speed: speed,      
-            energy: startEnergy 
-        };
-    }
+        // Новые поля:
+        speed: speed,      
+        energy: startEnergy 
+    };
+}
 
     // === Вспомогательная функция: выбор формы прилагательного ===
     function getAdjectiveForm(adjObj, gender, plural) {
