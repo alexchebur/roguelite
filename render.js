33d002d3406ec4becafc61d0e1fa9563895e3e84
alexchebur <93209745+alexchebur@users.noreply.github.com>
2026-06-14 23:10:18 +0500

    Update render.js

--- render.js
+++ render.js
@@ -735,6 +735,7 @@ const RenderModule = (function() {
         addBlinkEffect,
         addProjectileEffect,
         updateQuestBriefing,
+        drawShopWindow, // <--- ДОБАВИТЬ ЭТУ СТРОКУ
         COLS,
         ROWS,
         _ctx: null, 
