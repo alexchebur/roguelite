c319959c2c5148d49e5296c112a9fe7599dca473
alexchebur <93209745+alexchebur@users.noreply.github.com>
2026-08-06 10:42:36 +0500

    Update render.js

--- render.js
+++ render.js
@@ -1115,6 +1115,7 @@ const RenderModule = (function() {
         updateQuestBriefing,
         renderShopUI,
         renderQuestUI,
+        renderFullInventory,
         showShopStatus,
         COLS,
         ROWS,
